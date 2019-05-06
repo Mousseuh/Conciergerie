35a6f93cf2ba59cb842b58845464a8bbba63930f
Mousseuh <mael.mousson@gmail.com>
2019-05-06 11:48:16 +0200

    add unit test on user

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -12,10 +12,30 @@ class UserTest extends TestCase
     {
 
         $user = new User();
-        $user->setEmail('This is not an email');
+        $user->setEmail('This is not an email')
+            ->setPassword('test');
 
         $violations = $validator->validate($user);
 
         $this->assertCount(1, $violations);
+        $expected = [
+            'L\'email rentré n\'est pas valide',
+            'Votre mot de passe doit faire un minimum de 6 caractères',
+        ];
+
+        $messages = $this->getViolationMessages($violations);
+
+        $this->assertEquals($expected, $messages);
+    }
+
+    public function testValidationSuccess()
+    {
+        $user = new User();
+        $user->setEmail('test@test.com')
+            ->setPassword('test12');
+
+        $violations = $this->validator->validate($user);
+
+        $this->assertCount(0, $violations);
     }
 }
